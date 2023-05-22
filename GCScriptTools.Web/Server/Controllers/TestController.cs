ce9dbea0ba53617c353a0976a34f51623df54423
Gustavo Costa <gcs5stars@gmail.com>
2023-05-22 08:56:34 -0300

    Bug fixes and performance improvements

---
+++ GCScriptTools.Web/Server/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GCScriptTools.Web.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet] public ActionResult<string> Get() {
+        return "Teste...";
+    }
+}
