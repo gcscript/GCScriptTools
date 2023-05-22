96f6827fe683936a8223159c4dd481a3d3645b41
Henrique Clausing <hclausing@hotmail.com>
2023-05-22 13:58:24 -0300

    Refactoring das API's de Produtos, isolando por classes cada caminho de API (produtos, clientes, etc).

--- GCScriptTools.Web/Server/Apis/Mappings/ProductsMapper.cs
+++ GCScriptTools.Web/Server/Apis/Mappings/ProductsMapper.cs
@@ -4,9 +4,9 @@ public static class ProductsMapper
 {
     public static WebApplication MapProducts(this WebApplication app )
     {
-        app.MapGet("api/Product/GetFromMinimal", () => new Shared.Models.Product() { Id = Guid.NewGuid(), Name = "Minimal Product" });
-        
-        app.MapGet("api/Product/GetFromMinimal2", () => new Shared.Models.Product() { Id = Guid.NewGuid(), Name = "Minimal Product 2" });
+        app.MapGet("api/Product/GetFromMinimal", () => ProductsApis.Get());
+
+        app.MapGet("api/Product/GetFromMinimal2", () => ProductsApis.Get2());
 
         return app;
     }
---
+++ GCScriptTools.Web/Server/Apis/ProductsApis.cs
@@ -0,0 +1,12 @@
+﻿using GCScriptTools.Web.Shared.Models;
+
+namespace GCScriptTools.Web.Server.Apis
+{
+    public static class ProductsApis
+    {
+        internal static Product Get() => new Product() { Id = Guid.NewGuid(), Name = "Minimal Product" };
+
+        internal static Product Get2() => new Product() { Id = Guid.NewGuid(), Name = "Minimal Product 2" };
+
+    }
+}
